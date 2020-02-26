0cb943efcfc0f2b49c62ab82c18efa0304c920cb
Miles Frain <miles.frain@colorado.edu>
2020-02-26 12:31:22 -0800

    Rewrite Ch12 as "Asynchronous Effects"

--- exercises/chapter12/test/Main.purs
+++ exercises/chapter12/test/Main.purs
@@ -1,11 +1,148 @@
 module Test.Main where
 
 import Prelude
-
+import Test.Copy (copyFile)
+import Test.HTTP (getUrl)
+import Test.NoPeeking.Solutions  -- Note to reader: Delete this line
+import Test.MySolutions
+import Data.Array ((..))
+import Data.Bifunctor (lmap)
+import Data.Either (Either(..))
+import Data.Foldable (for_)
+import Data.Maybe (Maybe(..))
+import Data.Set as Set
+import Data.String (Pattern(..), split)
 import Effect (Effect)
-import Effect.Class.Console (log)
+import Effect.Exception (message)
+import Node.Encoding (Encoding(..))
+import Node.FS.Aff (readTextFile, readdir, unlink)
+import Node.Path (FilePath)
+import Node.Path as Path
+import Test.Unit (TestSuite, suite, test)
+import Test.Unit.Assert as Assert
+import Test.Unit.Main (runTest)
 
 main :: Effect Unit
-main = do
-  log "🍝"
-  log "You should add some tests."
+main =
+  runTest do
+    test "setup" do
+      -- Clear test output directory
+      files <- readdir outDir
+      for_ files \f -> unlink $ fp [ outDir, f ]
+    runChapterExamples
+    {-  Move this block comment starting point to enable more tests
+-}
+    -- Note to reader: Delete this line to expand comment block
+    test "concatenateFiles" do
+      let
+        inFoo = fp [ inDir, "foo.txt" ]
+
+        inBar = fp [ inDir, "bar.txt" ]
+
+        outFooBar = fp [ outDir, "foobar.txt" ]
+      concatenateFiles inFoo inBar outFooBar
+      -- Check for valid concat
+      inFooTxt <- readTextFile UTF8 inFoo
+      inBarTxt <- readTextFile UTF8 inBar
+      outFooBarTxt <- readTextFile UTF8 outFooBar
+      Assert.equal (inFooTxt <> inBarTxt) outFooBarTxt
+    test "concatenateMany" do
+      let
+        inFiles = map (\i -> fp [ inDir, "many", "file" <> show i <> ".txt" ]) $ 1 .. 9
+
+        outFile = fp [ outDir, "manyConcat.txt" ]
+
+        expectedOutFile = fp [ inDir, "manyConcat.txt" ]
+      concatenateMany inFiles outFile
+      -- Check for valid concat
+      actualOutTxt <- readTextFile UTF8 outFile
+      expectedOutTxt <- readTextFile UTF8 expectedOutFile
+      Assert.equal expectedOutTxt actualOutTxt
+    suite "countCharacters" do
+      test "exists" do
+        chars <- countCharacters $ fp [ inDir, "foo.txt" ]
+        Assert.equal (Right 41) $ lmap message chars
+      test "missing" do
+        chars <- countCharacters $ fp [ inDir, "foof.txt" ]
+        Assert.equal (Left "ENOENT: no such file or directory, open 'test/data/foof.txt'") $ lmap message chars
+    test "writeGet" do
+      let
+        outFile = fp [ outDir, "user.txt" ]
+
+        expectedOutFile = fp [ inDir, "user.txt" ]
+      writeGet "https://reqres.in/api/users/1" outFile
+      -- Check for valid write
+      actualOutTxt <- readTextFile UTF8 outFile
+      expectedOutTxt <- readTextFile UTF8 expectedOutFile
+      Assert.equal expectedOutTxt actualOutTxt
+    test "concatenateManyParallel" do
+      let
+        inFiles = map (\i -> fp [ inDir, "many", "file" <> show i <> ".txt" ]) $ 1 .. 9
+
+        outFile = fp [ outDir, "manyConcatParallel.txt" ]
+
+        expectedOutFile = fp [ inDir, "manyConcat.txt" ]
+      concatenateManyParallel inFiles outFile
+      -- Check for valid concat
+      actualOutTxt <- readTextFile UTF8 outFile
+      expectedOutTxt <- readTextFile UTF8 expectedOutFile
+      Assert.equal expectedOutTxt actualOutTxt
+    suite "getWithTimeout" do
+      test "valid site" do
+        let
+          expectedOutFile = fp [ inDir, "user.txt" ]
+        actual <- getWithTimeout 1000.0 "https://reqres.in/api/users/1"
+        expected <- Just <$> readTextFile UTF8 expectedOutFile
+        Assert.equal expected actual
+      test "no response" do
+        actual <- getWithTimeout 10.0 "https://example.com:81"
+        Assert.equal Nothing actual
+    suite "recurseFiles" do
+      let
+        recurseDir = fp [ inDir, "tree" ]
+      test "many files" do
+        expectedTxt <- readTextFile UTF8 $ fp [ recurseDir, "expected.txt" ]
+        let
+          expected = Path.normalize <$> split (Pattern "\n") expectedTxt
+        actual <- recurseFiles $ fp [ recurseDir, "root.txt" ]
+        let
+          actualRelative = map (\f -> Path.relative recurseDir f) actual
+        Assert.equal (Set.fromFoldable expected) $ Set.fromFoldable actualRelative
+      test "one file" do
+        let
+          file = fp [ recurseDir, "c/unused.txt" ]
+
+          expected = [ file ]
+        actual <- recurseFiles file
+        Assert.equal (Set.fromFoldable expected) $ Set.fromFoldable actual
+
+{- Note to reader: Delete this line to expand comment block
+-}
+runChapterExamples :: TestSuite
+runChapterExamples = do
+  test "copyFile" do
+    let
+      inFoo = fp [ inDir, "foo.txt" ]
+
+      outFoo = fp [ outDir, "foo.txt" ]
+    copyFile inFoo outFoo
+    -- Check for valid copy
+    inFooTxt <- readTextFile UTF8 inFoo
+    outFooTxt <- readTextFile UTF8 outFoo
+    Assert.equal inFooTxt outFooTxt
+  test "getUrl" do
+    let
+      expectedOutFile = fp [ inDir, "user.txt" ]
+    str <- getUrl "https://reqres.in/api/users/1"
+    -- Check for valid read
+    expectedOutTxt <- readTextFile UTF8 expectedOutFile
+    Assert.equal expectedOutTxt str
+
+fp :: Array FilePath -> FilePath
+fp = Path.concat
+
+inDir :: FilePath
+inDir = fp [ "test", "data" ]
+
+outDir :: FilePath
+outDir = fp [ "test", "data-out" ]
---
+++ exercises/chapter12/test/MySolutions.purs
@@ -0,0 +1,5 @@
+module Test.MySolutions where
+
+import Prelude
+
+-- Note to reader: Add you solutions to this file
