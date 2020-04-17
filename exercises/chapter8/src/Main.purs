abc25c39c4dccd0d673b78248396106a2c6d6507
Miles Frain <miles.frain@colorado.edu>
2020-04-17 13:13:28 -0600

    Apply purty formatting

--- exercises/chapter8/src/Main.purs
+++ exercises/chapter8/src/Main.purs
@@ -1,7 +1,6 @@
 module Main where
 
 import Prelude
-
 import Data.AddressBook (Address(..), Person(..), PhoneNumber(..), examplePerson)
 import Data.AddressBook.Validation (Errors, validatePerson')
 import Data.Maybe (Maybe(..), fromMaybe)
@@ -20,10 +19,10 @@ import Web.HTML (window)
 import Web.HTML.HTMLDocument (toNonElementParentNode)
 import Web.HTML.Window (document)
 
-type State =
-  { person :: Person
-  , errors :: Errors
-  }
+type State
+  = { person :: Person
+    , errors :: Errors
+    }
 
 initialState :: State
 initialState =
@@ -33,10 +32,10 @@ initialState =
 
 data Action
   = UpdateFirstName (Maybe String)
-  | UpdateLastName  (Maybe String)
-  | UpdateStreet    (Maybe String)
-  | UpdateCity      (Maybe String)
-  | UpdateState     (Maybe String)
+  | UpdateLastName (Maybe String)
+  | UpdateStreet (Maybe String)
+  | UpdateCity (Maybe String)
+  | UpdateState (Maybe String)
   | UpdatePhoneNumber Int (Maybe String)
 
 reducer :: State -> Action -> State
@@ -46,46 +45,45 @@ reducer state@{ person: Person p@{ homeAddress: Address a } } action =
 
     newPerson = case action of
       UpdateFirstName (Just value) -> Person p { firstName = value }
-      UpdateLastName  (Just value) -> Person p { lastName  = value }
-      UpdateStreet    (Just value) -> Person p { homeAddress = Address a { street = value } }
-      UpdateCity      (Just value) -> Person p { homeAddress = Address a { city   = value } }
-      UpdateState     (Just value) -> Person p { homeAddress = Address a { state  = value } }
+      UpdateLastName (Just value) -> Person p { lastName = value }
+      UpdateStreet (Just value) -> Person p { homeAddress = Address a { street = value } }
+      UpdateCity (Just value) -> Person p { homeAddress = Address a { city = value } }
+      UpdateState (Just value) -> Person p { homeAddress = Address a { state = value } }
       UpdatePhoneNumber index (Just value) -> Person p { phones = fromMaybe p.phones $ modifyAt index (updatePhoneNumber value) p.phones }
       _ -> state.person
   in
     case validatePerson' newPerson of
       Left errors -> state { person = newPerson, errors = errors }
-      Right _     -> state { person = newPerson, errors = [] }
+      Right _ -> state { person = newPerson, errors = [] }
 
 formField :: String -> String -> String -> (Maybe String -> Action) -> (Action -> Effect Unit) -> R.JSX
 formField name hint value actionConstructor dispatch =
   D.div
     { className: "form-group"
     , children:
-      [ D.label
-        { className: "col-sm-2 control-label"
-        , children: [ D.text name ]
-        }
-      , D.div
-        { className: "col-sm-3"
-        , children:
-          [ D.input
-            { className: "form-control"
-            , placeholder: hint
-            , value
-            , onChange: handler targetValue \v -> dispatch $ actionConstructor v
+        [ D.label
+            { className: "col-sm-2 control-label"
+            , children: [ D.text name ]
             }
-          ]
-        }
-      ]
+        , D.div
+            { className: "col-sm-3"
+            , children:
+                [ D.input
+                    { className: "form-control"
+                    , placeholder: hint
+                    , value
+                    , onChange: handler targetValue \v -> dispatch $ actionConstructor v
+                    }
+                ]
+            }
+        ]
     }
 
 renderPhoneNumber :: (Action -> Effect Unit) -> PhoneNumber -> Int -> R.JSX
 renderPhoneNumber dispatch (PhoneNumber phone) index =
   let
     actionConstructor :: Maybe String -> Action
-    actionConstructor ms =
-      UpdatePhoneNumber index ms
+    actionConstructor ms = UpdatePhoneNumber index ms
   in
     formField (show phone."type") "XXX-XXX-XXXX" phone.number actionConstructor dispatch
 
@@ -94,6 +92,7 @@ renderValidationError err = D.li_ [ D.text err ]
 
 renderValidationErrors :: Errors -> Array R.JSX
 renderValidationErrors [] = []
+
 renderValidationErrors xs =
   [ D.div
       { className: "alert alert-danger"
@@ -106,43 +105,43 @@ mkAddressBookApp = do
   component "AddressBookApp" \props -> R.do
     { person: Person person@{ homeAddress: Address address }, errors } /\ dispatch <-
       useReducer initialState reducer
-
-    pure $
-      D.div
-        { className: "container"
-        , children:
-          [ D.div
-            { className: "row"
-            , children: renderValidationErrors errors
-            }
-          , D.div
-            { className: "row"
-            , children:
-              [ D.form
-                { className: "form-horizontal"
-                , children:
-                  [ D.h3_ [ D.text "Basic Information" ]
-                  , formField "First Name" "First Name" person.firstName UpdateFirstName dispatch
-                  , formField "Last Name"  "Last Name"  person.lastName  UpdateLastName  dispatch
-                  , D.h3_ [ D.text "Address" ]
-                  , formField "Street" "Street" address.street UpdateStreet dispatch
-                  , formField "City"   "City"   address.city   UpdateCity   dispatch
-                  , formField "State"  "State"  address.state  UpdateState  dispatch
-                  , D.h3_ [ D.text "Contact Information" ]
-                  ] <>
-                    zipWith (renderPhoneNumber dispatch) person.phones (0 .. length person.phones)
-                }
+    pure
+      $ D.div
+          { className: "container"
+          , children:
+              [ D.div
+                  { className: "row"
+                  , children: renderValidationErrors errors
+                  }
+              , D.div
+                  { className: "row"
+                  , children:
+                      [ D.form
+                          { className: "form-horizontal"
+                          , children:
+                              [ D.h3_ [ D.text "Basic Information" ]
+                              , formField "First Name" "First Name" person.firstName UpdateFirstName dispatch
+                              , formField "Last Name" "Last Name" person.lastName UpdateLastName dispatch
+                              , D.h3_ [ D.text "Address" ]
+                              , formField "Street" "Street" address.street UpdateStreet dispatch
+                              , formField "City" "City" address.city UpdateCity dispatch
+                              , formField "State" "State" address.state UpdateState dispatch
+                              , D.h3_ [ D.text "Contact Information" ]
+                              ]
+                                <> zipWith (renderPhoneNumber dispatch) person.phones (0 .. length person.phones)
+                          }
+                      ]
+                  }
               ]
-            }
-          ]
-        }
+          }
 
 main :: Effect Unit
 main = do
   container <- getElementById "container" =<< (map toNonElementParentNode $ document =<< window)
   case container of
     Nothing -> throw "Container element not found."
-    Just c  -> do
+    Just c -> do
       addressBookApp <- mkAddressBookApp
-      let app = element addressBookApp {}
+      let
+        app = element addressBookApp {}
       render app c
