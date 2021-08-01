a9c757fa4fb3f555e9639ebf85b6713288416b5e
nathan wilson <ntwilson@gmail.com>
2021-07-31 23:25:27 -0400

    Anchors for all final code blocks for chapter 3

--- exercises/chapter3/src/Data/AddressBook.purs
+++ exercises/chapter3/src/Data/AddressBook.purs
@@ -1,3 +1,4 @@
+-- ANCHOR: imports
 module Data.AddressBook where
 
 import Prelude
@@ -5,36 +6,63 @@ import Prelude
 import Control.Plus (empty)
 import Data.List (List(..), filter, head)
 import Data.Maybe (Maybe)
+-- ANCHOR_END: imports
 
+-- ANCHOR: Address
 type Address =
   { street :: String
   , city   :: String
   , state  :: String
   }
+-- ANCHOR_END: Address
 
+-- ANCHOR: Entry
 type Entry =
   { firstName :: String
   , lastName  :: String
   , address   :: Address
   }
+-- ANCHOR_END: Entry
 
+-- ANCHOR: AddressBook
 type AddressBook = List Entry
+-- ANCHOR_END: AddressBook
 
+-- ANCHOR: showAddress
 showAddress :: Address -> String
-showAddress addr = addr.street <> ", " <> addr.city <> ", " <> addr.state
+showAddress addr = addr.street <> ", " <> 
+                   addr.city <> ", " <> 
+                   addr.state
+-- ANCHOR_END: showAddress
 
+-- ANCHOR: showEntry_signature
 showEntry :: Entry -> String
-showEntry entry = entry.lastName <> ", " <> entry.firstName <> ": " <> showAddress entry.address
+-- ANCHOR_END: showEntry_signature
+-- ANCHOR: showEntry_implementation
+showEntry entry = entry.lastName <> ", " <> 
+                  entry.firstName <> ": " <> 
+                  showAddress entry.address
+-- ANCHOR_END: showEntry_implementation
 
+-- ANCHOR: emptyBook
 emptyBook :: AddressBook
 emptyBook = empty
+-- ANCHOR_END: emptyBook
 
+-- ANCHOR: insertEntry
+-- ANCHOR: insertEntry_signature
 insertEntry :: Entry -> AddressBook -> AddressBook
-insertEntry = Cons
+-- ANCHOR_END: insertEntry_signature
+insertEntry = Cons 
+-- ANCHOR_END: insertEntry
 
+-- ANCHOR: findEntry_signature
 findEntry :: String -> String -> AddressBook -> Maybe Entry
+-- ANCHOR_END: findEntry_signature
+-- ANCHOR: findEntry_implementation
 findEntry firstName lastName = head <<< filter filterEntry
   where
+-- ANCHOR_END: findEntry_implementation
   filterEntry :: Entry -> Boolean
   filterEntry entry = entry.firstName == firstName && entry.lastName == lastName
 
