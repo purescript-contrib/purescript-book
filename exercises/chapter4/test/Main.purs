c54406d1a1d2d462d3984e1b01512c38fb163c19
Miles Frain <miles.frain@colorado.edu>
2021-09-06 13:57:20 -0700

    Ch4 improve factors tests

--- exercises/chapter4/test/Main.purs
+++ exercises/chapter4/test/Main.purs
@@ -5,8 +5,10 @@ import Test.Examples
 import Test.MySolutions
 import Test.NoPeeking.Solutions  -- This line should have been automatically deleted by resetSolutions.sh. See Chapter 2 for instructions.
 import Data.Array (sort)
+import Data.Foldable (sequence_)
 import Data.Maybe (Maybe(..))
 import Data.Path (Path(..), filename, root)
+import Data.Tuple.Nested ((/\))
 import Effect (Effect)
 import Test.Unit (TestSuite, suite, test)
 import Test.Unit.Assert (assert, assertFalse)
@@ -210,15 +212,23 @@ runChapterExamples =
     test "length" do
       Assert.equal 3
         $ length [ 0, 0, 0 ]
-    test "factors" do
-      Assert.equal [ [ 1, 10 ], [ 2, 5 ] ]
-        $ factors 10
-    test "factorsV2" do
-      Assert.equal [ [ 1, 10 ], [ 2, 5 ] ]
-        $ factorsV2 10
-    test "factorsV3" do
-      Assert.equal [ [ 1, 10 ], [ 2, 5 ] ]
-        $ factorsV3 10
+    sequence_ do
+      name /\ f <-
+        [ "factors" /\ factors
+        , "factorsV2" /\ factorsV2
+        , "factorsV3" /\ factorsV3
+        ]
+      n /\ xs <-
+        [ 1 /\ [[1,1]]
+        , 2 /\ [[1,2]]
+        , 3 /\ [[1,3]]
+        , 4 /\ [[1,4],[2,2]]
+        , 10 /\ [[1,10],[2,5]]
+        , 100 /\ [[1,100],[2,50],[4,25],[5,20],[10,10]]
+        ]
+      pure $ test (name <> " " <> show n) do
+        Assert.equal (sort $ map sort xs)
+          $ sort $ map sort f n
     test "factTailRec" do
       Assert.equal 120
         $ factTailRec 5 1
