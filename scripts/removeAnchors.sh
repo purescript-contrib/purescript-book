47b42434e8ba55913389b6534fad9aa07299c802
Yuriy Syrovetskiy <git-orbi@cblp.su>
2021-11-13 12:08:36 +0300

    Fix scripts/removeAnchors.sh on Mac

--- scripts/removeAnchors.sh
+++ scripts/removeAnchors.sh
@@ -8,7 +8,7 @@ set -x
 set -e
 
 # All .purs & .js files in the src/ and test/ directories of chapter exercises.
-FILES=$(find . -regextype posix-extended -regex '\./exercises/chapter[0-9]{1,2}/(src|test)/.*\.(purs|js)' -type f)
+FILES=$(find -E . -regex '\./exercises/chapter[0-9]{1,2}/(src|test)/.*\.(purs|js)' -type f)
 
 for f in $FILES; do
   # Delete lines starting with an 'ANCHOR' comment
