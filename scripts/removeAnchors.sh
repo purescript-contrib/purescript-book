d8e1bad4048cfbe05b826f424887944712f1ddb7
Wiktor Żurawik <w.zurawik@nullthepointer.com>
2023-07-07 09:35:31 +0200

    [Scripts] Make removeAnchors script support Darwin's BSD find

--- scripts/removeAnchors.sh
+++ scripts/removeAnchors.sh
@@ -2,13 +2,20 @@
 
 # This script removes all code anchors to improve readability
 
+FIND_FILES_PATTERN='\./exercises/chapter[0-9]{1,2}/(src|test)/.*\.(purs|js)'
+OS=$(uname)
+
 # Echo commands to shell
 set -x
 # Exit on first failure
 set -e
 
 # All .purs & .js files in the src/ and test/ directories of chapter exercises.
-FILES=$(find . -regextype posix-extended -regex '\./exercises/chapter[0-9]{1,2}/(src|test)/.*\.(purs|js)' -type f)
+if [[ "$OS" == 'Darwin' ]]; then
+    FILES=$(find -E . -regex "${FIND_FILES_PATTERN}" -type f)
+else
+    FILES=$(find . -regextype posix-extended -regex "${FIND_FILES_PATTERN}" -type f)
+fi
 
 for f in $FILES; do
   # Delete lines starting with an 'ANCHOR' comment
